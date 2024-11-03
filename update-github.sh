3d3da95bd172e7a569286b52bcc684b3b4684e8f
wielandsmith <monica@clu.cool>
2024-11-03 07:55:52 -0400

    Update update-github.sh

--- update-github.sh
+++ update-github.sh
@@ -1,1 +1,16 @@
- 
+#!/bin/bash
+
+# Add all changes
+git add .
+
+# Get the commit message as an argument
+commit_message="$1"
+
+# If no message provided, use a default
+if [ -z "$commit_message" ]; then
+    commit_message="Update from conversation with AI assistant"
+fi
+
+# Commit and push
+git commit -m "$commit_message"
+git push 
